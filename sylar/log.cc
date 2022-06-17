576793c900023a8274ad9021429e1ea242d117a8
zzuyb <1325855326@qq.com>
2022-06-18 00:11:17 +0800

    617

--- sylar/log.cc
+++ sylar/log.cc
@@ -2,9 +2,30 @@
 #include<iostream>
 #include<vector>
 namespace sylar{
-Logger::Logger(const std::string& name = "root"){
 
+const char*LogLevel::ToString(LogLevel::Level level) {
+    switch(level){
+#define XX(name) \
+    case LogLevel::name:\
+        return #name; \
+        break;
+    
+    XX(DEBUG);
+    XX(INFO);
+    XX(WARN);
+    XX(ERROR);
+    XX(FATAL);
+
+#undef XX
+    default:
+        return "UNKNOW";
+    }
+    return "UNKNOW";
 }
+
+Logger::Logger(const std::string& name)
+    :m_name(name){}
+
 
 void Logger::log(LogLevel::Level level, const LogEvent::ptr& event){
     if(level>=m_level){
@@ -45,7 +66,7 @@ FileLogAppender::FileLogAppender(const std::string& filename){
 }
 void FileLogAppender::log(LogLevel::Level level, const LogEvent::ptr& event){
     if(level>=m_level){
-        m_fileofs<<m_formatter->format(event);
+        m_fileofs<<m_formatter->format(level,event);
     }
 }
 
@@ -58,16 +79,16 @@ bool FileLogAppender::reopen(){
 }
 void StdoutLogAppender::log(LogLevel::Level level, const LogEvent::ptr& event){
     if(level >= m_level){
-        std::cout << m_formatter->format(event) << std::endl;
+        std::cout << m_formatter->format(level,event) << std::endl;
     }
 }
 LogFormatter::LogFormatter(const std::string& pattern)
     :m_pattern(pattern){}
 
-std::string LogFormatter::format(const LogEvent::ptr& ev){
+std::string LogFormatter::format(LogLevel::Level level,const LogEvent::ptr& ev){
     std::stringstream ss;
     for(auto& i : m_items){
-        i->format(ss,ev);
+        i->format(ss,level,ev);
     }
     return ss.str();
 }
@@ -75,36 +96,99 @@ std::string LogFormatter::format(const LogEvent::ptr& ev){
 void LogFormatter::init(){
     //str,format,type
     std::vector<std::tuple<std::string,std::string,int>> vec;
-    std::string str;
+    std::string nstr;
     for(size_t i=0;i<m_pattern.size();++i){
-        if(m_pattern[i] == '%'){
-            if(i+1 < m_pattern.size() && m_pattern[i+1] == '%'){
-                str.push_back(m_pattern[i]);
-                str.push_back(m_pattern[i+1]);
-                i++;
-                vec.push_back(std::make_tuple(str,str,0));
-                str.clear();
-            }else{
-                str.push_back(m_pattern[i]);
-                if(i+1 < m_pattern.size() && m_pattern[i+1] == '{'){
-                    i++;
-                    while(i < m_pattern.size() && m_pattern[i] != '}'){
-                        str.push_back(m_pattern[i]);
-                        i++;
-                    }
-                    if(i < m_pattern.size()){
-                        str.push_back(m_pattern[i]);
-                        vec.push_back(std::make_tuple(str,str,0));
-                        str.clear();
-                    }
-                }else{
-                    vec.push_back(std::make_tuple(str,str,0));
-                    str.clear();
+            if(m_pattern[i]!='%'){
+                nstr.append(1,m_pattern[i]);
+                continue;
+            }
+
+            if((i+1)<m_pattern.size()){
+                if(m_pattern[i+1]=='%'){
+                    nstr.append(1,'%');
+                    continue;
                 }
             }
-        }else{
-            str.push_back(m_pattern[i]);
+            size_t n=i+1;
+            int fmt_status=0;
+            size_t fmt_begin=0;
+
+            std::string str;
+            std::string fmt;
+
+            while(n<m_pattern.size()){
+                if((!fmt_status)&&isalpha(m_pattern[n])&&(m_pattern[n]!='{')&&(m_pattern[n]!='}')){
+                    str=m_pattern.substr(i+1,n-i-1);
+                    break;
+                }
+
+                if(fmt_status==0){
+                        if(m_pattern[n]=='{'){
+                        str=m_pattern.substr(i+1,n-i-1);
+                        fmt_status=1;
+                        fmt_begin=n;
+                        ++n;
+                        continue;
+                    }
+                }
+                if(fmt_status==1){
+                    if(m_pattern[n]=='}'){
+                        fmt=m_pattern.substr(fmt_begin+1,n-fmt_begin-1);
+                        fmt_status=0;
+                        ++n;
+                        break;
+                    }
+                }
+                ++n;
+                if(n==m_pattern.size()){
+                    if(str.empty()){
+                        str=m_pattern.substr(i+1);
+                    }
+                }
+            }
+            if(fmt_status==0){
+                if(!nstr.empty()){
+                    vec.push_back(std::make_tuple(nstr,std::string(),0));
+                    nstr.clear();
+                }
+                vec.push_back(std::make_tuple(str,fmt,1));
+                i=n-1;
+            }else if(fmt_status==1){
+                std::cout<<"pattern parse error: "<<m_pattern<<" - "<<m_pattern.substr(i)<<std::endl;
+                m_error=true;
+                vec.push_back(std::make_tuple("<<pattern_error>>",fmt,0));
+            }
         }
+
+        if(!nstr.empty()){
+            vec.push_back(std::make_tuple(nstr,"",0));
+        } 
+        //%m -- 消息体
+        //%p -- level
+        //%r -- 启动后的时间
+        //%c -- 日志名称
+        //%t -- 线程id
+        //%n -- 回车换行
+        //%d -- 时间
+        //%f -- 文件名
+        //%l -- 行号
     }
-}
+
+class MessageFormatItem : public LogFormatter::FormatItem{
+    public:
+        void format(std::ostream& os, const LogEvent::ptr& ev) {
+            os<<ev->getContent();
+        }
+};
+
+class LevelFormatItem : public LogFormatter::FormatItem{
+    public:
+        void format(std::ostream& os, LogLevel::Level level,const LogEvent::ptr& ev) {
+            os<<LogLevel::ToString(level);
+        }
+};
+
+
+
+
 }
--- sylar/log.h
+++ sylar/log.h
@@ -12,10 +12,17 @@ class LogEvent{
     public:
         typedef std::shared_ptr<LogEvent> ptr;
         LogEvent();
+
+        const char* getFile() const {return m_file;}
+        int32_t getLine() const {return m_line;}
+        uint32_t getElapse() const {return m_elapse;}
+        uint32_t getThreadId() const {return m_threadId;}
+        uint32_t getFiberId() const {return m_fiberId;}
+        const std::string& getContent () const {return m_content;}
     private:
         const char* m_file=nullptr;//文件名
         int32_t m_line;//行号
-        uint32_t m_elapsed;//耗时
+        uint32_t m_elapse;//耗时
         uint32_t m_threadId;//线程id
         uint32_t m_fiberId;//协程id
         uint64_t m_time;//时间戳
@@ -25,12 +32,15 @@ class LogEvent{
 class LogLevel{
     public:
         enum Level{
+            UNKNOW=0, 
             DEBUG = 1,
             INFO = 2,
             WARN = 3,
             ERROR = 4,
             FATAL = 5,
         };
+
+        static const char* ToString(Level level);
 };
 //日志格式器
 class LogFormatter{
@@ -38,16 +48,17 @@ class LogFormatter{
         typedef std::shared_ptr<LogFormatter> ptr;
         LogFormatter(const std::string& pattern);
         
-        std::string format(const LogEvent::ptr& ev);
-    private:
+        std::string format(LogLevel::Level level, const LogEvent::ptr& ev);
+    public:
         class FormatItem{
             public:
                 typedef std::shared_ptr<FormatItem> ptr;
                 virtual ~FormatItem(){}
-                virtual void format(std::ostream os, const LogEvent::ptr& ev) = 0; 
+                virtual void format(std::ostream os,LogLevel::Level level, const LogEvent::ptr& ev) = 0; 
         };
         void init();  
     private:
+        bool m_error=false;
         std::string m_pattern;
         std::list<FormatItem::ptr> m_items;
 };
